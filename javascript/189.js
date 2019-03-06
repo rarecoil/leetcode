8ef5beb72f583d27ff6d2f6b27ebfb9a5e5c0234
rarecoil <rarecoil@users.noreply.github.com>
2019-03-05 22:54:19 -0800

    one more

---
+++ javascript/189.js
@@ -0,0 +1,47 @@
+// Number: 189
+// Title: Rotate Array
+
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// first try
+var rotate = function(nums, k) {
+    if (k === 0) return;
+    for (let i=0;i<k;i++) {
+        nums.unshift(nums.pop());
+    }
+};
+
+
+// came up with this idea in the middle of making the other,
+// but it doesn't actually work once the array wraps
+var rotateSlice = function(nums, k) {
+    if (k === 0) return;
+    // slice k off the back of the array
+    Array.prototype.unshift.apply(nums, nums.splice(k*-1, k));
+}
